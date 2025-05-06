554991f0f348a4966147670edd9c8f060c6ffbd4
DavidMPerich <davidmperich@gmail.com>
2025-05-05 17:51:54 -0700

    faq work

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ menu.addEventListener('click', () => {
     document.querySelector(".navbar").classList.toggle("change");
     document.querySelector(".landing-banner").classList.toggle("blur");
 });
+
+let accordion = document.querySelector('.accordionBox'),
+    accordionItem = document.querySelectorAll('.accordionItem');
+accordionItem.forEach(elem => {
+    elem.addEventListener('click', function (e) {
+        e.srcElement.parentElement.classList.toggle('active');
+    });
+});
